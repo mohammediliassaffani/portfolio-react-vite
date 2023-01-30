d84a40d9952508b8ed1c43adb558b3a8c9f0839a
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-31 00:57:22 +0100

    right-nav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -26,7 +26,7 @@ const Sidenav = () => {
             <span className="pl-4">Home</span>
           </a>
           <a
-            href="#main"
+            href="#skills"
             className="w-[75%]
              flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110"
           >
@@ -34,7 +34,7 @@ const Sidenav = () => {
             <span className="pl-4">Skills</span>
           </a>
           <a
-            href="#main"
+            href="#Works"
             className="w-[75%]
              flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110"
           >
@@ -42,7 +42,7 @@ const Sidenav = () => {
             <span className="pl-4">Projects</span>
           </a>
           <a
-            href="#main"
+            href="#Person"
             className="w-[75%]
              flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110"
           >
@@ -50,7 +50,7 @@ const Sidenav = () => {
             <span className="pl-4">Resume</span>
           </a>
           <a
-            href="#main"
+            href="#contact"
             className="w-[75%]
              flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110"
           >
@@ -71,25 +71,25 @@ const Sidenav = () => {
             <AiOutlineHome size={20} />
           </a>
           <a
-            href="#main"
+            href="#skills"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4  cursor-pointer hover:scale-110 duration-300 "
           >
-            <AiOutlineHome size={20} />
+            <GiPowerLightning size={20} />
           </a>
           <a
-            href="#main"
+            href="#work"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4  cursor-pointer hover:scale-110 duration-300 "
           >
-            <AiOutlineHome size={20} />
+            <GrProjects size={20} />
           </a>
           <a
-            href="#main"
+            href="#person"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4  cursor-pointer hover:scale-110 duration-300 "
           >
-            <AiOutlineHome size={20} />
+            <BsPerson size={20} />
           </a>
           <a
-            href="#main"
+            href="#contact"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4  cursor-pointer hover:scale-110 duration-300 "
           >
             <AiOutlineMail size={20} />
