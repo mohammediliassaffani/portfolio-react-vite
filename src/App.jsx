d533a3a351b1bff4f65fdb03ebef45301d90b4b0
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-31 00:03:06 +0100

    start-sideNav

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Sidenav from "./components/Sidenav";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Sidenav />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
 
 const Sidenav = () => {
-  return <div></div>;
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
+  return (
+    <div>
+      <AiOutlineMenu
+        onClick={handleNav}
+        className="absolute top-4 right-4 z-[99] md:hidden"
+      />
+      {nav ? (
+        <div className="fixd w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+          <a href="#main" className="w-[75%] flex justify-center ">
+            <AiOutlineHome size={20} />
+            <span>Home</span>
+          </a>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 };
 
 export default Sidenav;
