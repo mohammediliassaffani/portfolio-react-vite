6a01e7114a423faece000d20c2e4850b25e0bea8
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-31 00:18:35 +0100

    test-nav

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -14,9 +14,45 @@ const Sidenav = () => {
       />
       {nav ? (
         <div className="fixd w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-          <a href="#main" className="w-[75%] flex justify-center ">
+          <a
+            href="#main"
+            className="w-[75%]
+             flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110"
+          >
             <AiOutlineHome size={20} />
-            <span>Home</span>
+            <span className="pl-4">Home</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%]
+             flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Skills</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%]
+             flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Projects</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%]
+             flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Resume</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%]
+             flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110"
+          >
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Contact</span>
           </a>
         </div>
       ) : (
