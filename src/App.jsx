f364bb4474c962c84d1af242af10d05faeec8384
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-31 01:11:38 +0100

    pic-home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Sidenav from "./components/sidenav/Sidenav";
+import Main from "./components/home/Main";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Sidenav />
+      <Main />
     </div>
   );
 }
--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Home } from "../../assets/Home.png";
+import Home from "../../assets/Home.png";
 
 const Main = () => {
   return (
